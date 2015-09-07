65b894cd31c70bd5510201eaea84074285a1bdba
Rubén Díaz Martínez <rubendm92@gmail.com>
2015-09-07 18:24:40 +0100

    Refactor

--- core/test/captiom/core/use_cases/AcceptedSearchPatientCommand.java
+++ core/test/captiom/core/use_cases/AcceptedSearchPatientCommand.java
@@ -3,6 +3,7 @@ package captiom.core.use_cases;
 import captiom.core.model.Gender;
 import captiom.core.model.Patient;
 import captiom.core.model.PatientNotFound;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -12,9 +13,15 @@ import static org.hamcrest.core.Is.is;
 
 public class AcceptedSearchPatientCommand {
 
+	private SearchPatientCommand command;
+
+	@Before
+	public void setUp() {
+		command = new SearchPatientCommand();
+	}
+
 	@Test
 	public void should_return_patient_given_patient_id() {
-		SearchPatientCommand command = new SearchPatientCommand();
 		Patient patient = command.searchPatient("1111");
 		assertThat(patient.id, is("1111"));
 		assertThat(patient.dateOfBirth, is(LocalDate.of(1992, 6, 22)));
@@ -23,10 +30,6 @@ public class AcceptedSearchPatientCommand {
 
 	@Test(expected = PatientNotFound.class)
 	public void should_throw_patient_not_found_exception_when_patient_is_not_registered() {
-		SearchPatientCommand command = new SearchPatientCommand();
-		Patient patient = command.searchPatient("1113");
-		assertThat(patient.id, is("1113"));
-		assertThat(patient.dateOfBirth, is(LocalDate.of(1992, 10, 15)));
-		assertThat(patient.gender, is(Gender.FEMALE));
+		command.searchPatient("1113");
 	}
 }
