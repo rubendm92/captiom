d854214706372816ed41cb53ff2051d1094675eb
Rubén Díaz Martínez <rubendm92@gmail.com>
2015-09-12 13:50:05 +0100

    Throw exception on not implemented methods

--- core/src/captiom/core/model/patient/PatientService.java
+++ core/src/captiom/core/model/patient/PatientService.java
@@ -3,9 +3,10 @@ package captiom.core.model.patient;
 public class PatientService {
 
 	public Patient get(String id) {
-		return null;
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 
 	public void save(Patient patient) {
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 }
