37154e9156ee5079de23e13cd2acdc3192e379a9
Rubén Díaz Martínez <rubendm92@gmail.com>
2015-09-12 14:03:14 +0100

    Test when get returns no patient

--- core/test/captiom/core/model/patient/AcceptedPatientService.java
+++ core/test/captiom/core/model/patient/AcceptedPatientService.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static java.time.LocalDate.of;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -13,11 +15,22 @@ public class AcceptedPatientService {
 
 	@Test
 	public void should_return_patient_from_repository() {
-		PatientRepository repository = mock(PatientRepository.class);
 		String patientId = "4444";
 		Patient patient = new Patient(patientId, of(1992, 6, 22), Gender.MALE);
-		PatientService service = new PatientService(repository);
-		service.get(patientId);
+		PatientRepository repository = mock(PatientRepository.class);
 		when(repository.find(patientId)).thenReturn(Optional.of(patient));
+
+		PatientService service = new PatientService(repository);
+		assertThat(service.get(patientId).get(), is(patient));
+	}
+
+	@Test
+	public void should_return_empty_optional_when_patient_is_not_in_repository() {
+		String patientId = "4444";
+		PatientRepository repository = mock(PatientRepository.class);
+		when(repository.find(patientId)).thenReturn(Optional.empty());
+
+		PatientService service = new PatientService(repository);
+		assertThat(service.get(patientId).isPresent(), is(false));
 	}
 }
