f9c0830f4bc535784221f6abbfe75718d4cc3005
Rubén Díaz Martínez <rubendm92@gmail.com>
2015-10-25 16:25:49 +0000

    Implement push service using WebSockets. Handle routing with push messages

---
+++ server/src/captiom/server/infrastructure/PushService.java
@@ -0,0 +1,62 @@
+package captiom.server.infrastructure;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PushService extends WebSocketServer {
+
+	private final List<ConnectionOpened> connectionOpenedListeners = new ArrayList<>();
+
+	public PushService(int port) {
+		super(new InetSocketAddress(port));
+		start();
+	}
+
+	@Override
+	public void onOpen(WebSocket socket, ClientHandshake handshake) {
+		connectionOpenedListeners.forEach(ConnectionOpened::onOpen);
+	}
+
+	@Override
+	public void onClose(WebSocket socket, int code, String reason, boolean remote) {
+	}
+
+	@Override
+	public void onMessage(WebSocket socket, String message) {
+		notify(message);
+	}
+
+	@Override
+	public void onError(WebSocket socket, Exception e) {
+	}
+
+	public void addConnectionOpenedListener(ConnectionOpened connectionOpened) {
+		connectionOpenedListeners.add(connectionOpened);
+	}
+
+	public void notify(String text) {
+		connections().stream().forEach(connection -> connection.send(serialize(text, "")));
+	}
+
+	public void notify(String title, String content) {
+		connections().stream().forEach(connection -> connection.send(serialize(title, content)));
+	}
+
+	private String serialize(String title, String content) {
+		JsonObject object = new JsonObject();
+		object.addProperty("title", title);
+		object.add("content", new JsonParser().parse(content));
+		return object.toString();
+	}
+
+	public interface ConnectionOpened {
+		void onOpen();
+	}
+}
