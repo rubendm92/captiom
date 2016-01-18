2f52018afb8ed1ea08b45408cfe4ce389c1f6030
Rubén Díaz Martínez <rubendm92@gmail.com>
2016-01-18 17:28:00 +0000

    Clean up

--- server/src/captiom/server/controllers/ConfigureDeviceController.java
+++ server/src/captiom/server/controllers/ConfigureDeviceController.java
@@ -2,7 +2,6 @@ package captiom.server.controllers;
 
 import captiom.server.displays.DeviceDisplay;
 import captiom.server.infrastructure.DisplayService;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import spark.Request;
 import spark.Response;
@@ -10,22 +9,19 @@ import spark.Response;
 public class ConfigureDeviceController implements Controller {
 
 	private final DisplayService displayService;
-	private final Gson gson;
 
 	public ConfigureDeviceController(DisplayService displayService) {
 		this.displayService = displayService;
-		this.gson = new Gson();
 	}
 
 	@Override
 	public Object handle(Request request, Response response) throws Exception {
-		displayService.display(DeviceDisplay.class).configure(buildConfiguration(request.body()));
+		displayService.display(DeviceDisplay.class).configure(buildConfiguration(Controller.bodyFrom(request)));
 		response.status(200);
 		return "OK";
 	}
 
-	private DeviceDisplay.Configuration buildConfiguration(String body) {
-		JsonObject object = gson.fromJson(body, JsonObject.class);
-		return new DeviceDisplay.Configuration(object.get("deviceId").getAsString(), object.get("diopters").getAsDouble(), object.get("distance").getAsDouble() / 1000);
+	private DeviceDisplay.Configuration buildConfiguration(JsonObject body) {
+		return new DeviceDisplay.Configuration(body.get("deviceId").getAsString(), body.get("diopters").getAsDouble(), body.get("distance").getAsDouble() / 1000);
 	}
 }
